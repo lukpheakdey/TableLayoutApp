f138b356150391dae6e77aaff38a61b83c44201a
Luk Pheakdey <luk.pheakdey@gmail.com>
2020-09-02 22:18:32 -0400

    Make Dynamic add android code and name to Table Layout. Programmatically for TextView, TableLayout, TableRow

--- app/src/main/java/com/pheakdey/lesson4/MainActivity.kt
+++ app/src/main/java/com/pheakdey/lesson4/MainActivity.kt
@@ -2,8 +2,10 @@ package com.pheakdey.lesson4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TableRow
-import android.widget.Toast
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.view.marginLeft
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,16 @@ class MainActivity : AppCompatActivity() {
             val androidVersion = etAndroidVersion.text
             val androidCode = etAndroidCodeName.text
 
+            val textAndroidVersion = TextView(this)
+            textAndroidVersion.text = androidVersion
+            textAndroidVersion.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            //textAndroidVersion.setPadding(0,0,500,0)
+
+            val textAndroidCode = TextView(this)
+            textAndroidCode.text = androidCode
+            textAndroidCode.setPadding(280,0,0,0)
+            textAndroidCode.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+
             Toast.makeText(this, androidVersion, Toast.LENGTH_SHORT).show()
             Toast.makeText(this, androidCode, Toast.LENGTH_SHORT).show()
 
@@ -23,11 +35,14 @@ class MainActivity : AppCompatActivity() {
             val tableRow = TableRow(applicationContext)
 
             //set new tble row layout parameters
-            val layoutRarams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+            val layoutRarams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT)
             tableRow.layoutParams = layoutRarams
 
             //add values into row by calling addView()
-            //tableRow.addVi
+            tableRow.addView(textAndroidVersion)
+            tableRow.addView(textAndroidCode)
+
+            table_main.addView(tableRow)
 
         }
 
